345c5fab8e9c9800c070712ae038a6292fe1baa1
Enzo G. Almeida <myenzo121@gmail.com>
2023-06-06 19:54:49 -0300

    abrindo pygame

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+tamanho = (800,600)
+tela = pygame.display.set_mode( tamanho )
+branco = (255,255,255)
+
+running = True
+while running:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
+pygame.quit()
